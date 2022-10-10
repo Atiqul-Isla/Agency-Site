5f2fdde71702aaa32a86c5bb26afc0b45e46b9f2
Atiqul Islam <isla.atiqul@gmail.com>
2022-10-09 21:24:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Clients from "./components/Clients";
+import About from "./components/About"
+import Contact from "./components/Contact";
+import Services from "./components/Services"
+import Footer from "./components/Footer";
+
 
 function App() {
+  require('dotenv').config()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <About/>
+      <Services/>
+      <Clients />
+      <Contact />
+      <Footer />
     </div>
   );
 }
